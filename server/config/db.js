2d6d3dfc0100770816e3405aa60c07e85dd0a264
Hankyu Shin <brad0501@naver.com>
2020-10-30 12:24:30 -0400

    all

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://HK:<gksrb3721362>@mongodbserver.azeun.mongodb.net/<MongoDBServer>?retryWrites=true&w=majority"
+};
